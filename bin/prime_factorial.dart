613fdbcac03edac8632ae8811c8b3d7804597f25
SHIBIN0311 <toxicaza@gmai8l.com>
2024-02-01 13:20:15 +0530

    new

---
+++ bin/prime_factorial.dart
@@ -0,0 +1,23 @@
+void main() {
+  int num = 7;
+  List factors = [];
+
+  for (int i = 1; i <= num; i++) {
+    if (num % i == 0) {
+      factors.add(i);
+    }
+  }
+  if (factors.length > 2) {
+    print("$num is not a prime number");
+  } else {
+    print("$num  is a prime number ");
+    BigInt factorial(int num) {
+      if (num > 0) {
+        return BigInt.from(num) * factorial(num - 1);
+      } else {
+        return BigInt.one;
+      }
+    }
+  print(factorial(num));
+  }
+}
